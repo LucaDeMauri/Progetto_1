21d7a3decf6cee34821100ef28dc467b0223aa08
Luca De Mauri <luca.demauri.2006@gmail.com>
2025-01-29 13:29:52 +0100

    progetto1 finito

---
+++ login/rememberMe.js
@@ -0,0 +1,8 @@
+function validaForm() {
+    if (document.myForm.remember.checked){
+        window.alert("hai scelto di ricordarti per i prossimi acessi");
+    }
+    else {
+        window.alert("hai scelto di non ricordarti per i prossimi accessi");
+    }
+}
